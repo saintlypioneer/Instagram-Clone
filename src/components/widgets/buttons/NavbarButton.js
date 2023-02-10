19bca3957bbb9022d4631939a252944100df09a7
Ankit Agrawal <aggrawalankitsra@gmail.com>
2023-02-10 19:14:29 +0530

    initial commit

---
+++ src/components/widgets/buttons/NavbarButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NavbarButton() {
+  return (
+    <div>NavbarButton</div>
+  )
+}
+
+export default NavbarButton
---
+++ src/components/widgets/buttons/ViewStatusButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ViewStatusButton() {
+  return (
+    <div>ViewStatusButton</div>
+  )
+}
+
+export default ViewStatusButton
